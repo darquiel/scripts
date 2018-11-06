877c41694cf87c32357c6fb6fe527f7c651d40b7
Mark Andrews <mark.andrews@philips.com>
2018-11-06 12:48:53 -0800

    Made additional modifications.

--- PS1/sort-image.ps1
+++ PS1/sort-image.ps1
@@ -21,26 +21,39 @@ Param(
 Clear-Host
 Push-Location $sourceDir
 
-move-item *.zip ~/Downloads/CmPrsd/Z
-move-item *.rar ~/Downloads/CmPrsd/R
-
-gci -file | Rename-Item -NewName { $_.Name -replace "-fullview", "a" }
-gci -file | Rename-Item -NewName { $_.Name -replace "_", "-" }
-gci -file | Rename-Item -NewName { $_.Name -replace "-by-", "_by_" }
-
-$filesMine = Get-ChildItem -Name
-foreach ( $file in $filesMine ) {
-  $file | Where-Object { $_ -match '(?<=_by_)(.*)(?=-)' } | foreach-object {
-    $newPath = $targetDirRoot + $matches[0] + "/"
-    If(!(test-path $newPath))
-    {
-      $newPath
-      New-Item -type Directory -Force -Path $newPath
-    }
-    $fileName = "*" + $matches[0] + "*" 
-    move-item $fileName $newPath
-  }
+function MoveCompressedFiles {
+  move-item *.zip ~/Downloads/CmPrsd/Z
+  move-item *.rar ~/Downloads/CmPrsd/R
+  Write-Host "MoveCompressedFiles :: Completed"
 }
+
+function CleanFilenames {
+  gci -file | Rename-Item -NewName { $_.Name -replace "-fullview", "a" }
+  gci -file | Rename-Item -NewName { $_.Name -replace "_", "-" }
+  gci -file | Rename-Item -NewName { $_.Name -replace "-by-", "_by_" }
+  write-host "CleanFilenames :: Completed"
+}
+
+function SortImages {
+  $filesMine = Get-ChildItem -Name
+  foreach ( $file in $filesMine ) {
+    $file | Where-Object { $_ -match '(?<=_by_)(.*)(?=-)' } | foreach-object {
+      $newPath = $targetDirRoot + $matches[0] + "/"
+      If(!(test-path $newPath))
+      {
+        $newPath
+        New-Item -type Directory -Force -Path $newPath
+      }
+      $fileName = "*" + $matches[0] + "*" 
+      move-item $fileName $newPath
+    }
+  }
+  write-host "SortImages :: Completed"
+}
+
+MoveCompressedFiles
+CleanFilenames
+SortImages
 
 Pop-Location
 # end
