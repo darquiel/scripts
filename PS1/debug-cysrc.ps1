1f582e35e15fc453a18e6de9e09523f690b36c0e
mja <vamp_paradox@hotmail.com>
2021-09-13 10:46:23 -0700

    Modifications - to automation scripts

--- PS1/debug-cysrc.ps1
+++ PS1/debug-cysrc.ps1
@@ -65,7 +65,14 @@ function XtractFile {
   Write-Host "-=-=-=-=-=-=-=-=-=-=-=-"
   Write-Host " XtractFile :: Started "
   Write-Host "-=-=-=-=-=-=-=-=-=-=-=-"
- 
+  
+  # check for exisitng file and remove it / then unzip the file
+  ssh build@khazadum "if [ -f drop/4.0a.tar ]; then rm drop/4.0a.tar; fi"
+  ssh build@khazadum "gzip -f drop/4.0a.tar.gz"
+
+  # check for exisitng file (working directory) and remove it / then untar it
+  ssh build@khazadum "if [ -d 4.0a ]; then rm -rf 4.0a; fi"
+  ssh build@khazadum "tar -xvf drop/4.0a.tar"
 
   Write-Host "-=-=-=-=-=-=-=-=-=-=-=-=-"
   Write-Host " XtractFile :: Completed"
