fe27c913d4cd86c82f465eddcbe7b225e7b37786
mja <mark.andrews@serverobotics.com>
2022-04-18 21:47:47 -0700

    interim changes

--- SRV/cut-rc.ps1
+++ SRV/cut-rc.ps1
@@ -48,7 +48,9 @@ function Gen_rvr_branches {
   git branch release/rover/$rcName $brnchCmmtSKEL
   git switch release/rover/$rcName
   get-content -path release-manifest.yml -raw
-  (get-content -path release-manifest.yml -raw) -replace 'rover: "3.12.0"','rover: "' + $rcName + '"' > release-manifesta.yml
+  $rvrVersion = "rover: " + `"` + $rcName + `"`
+  $rvrVersion
+  (get-content -path release-manifest.yml -raw) -replace 'rover: "3.12.0"',"$rvrVersion" > release-manifesta.yml
   rm release-manifest.yml
   rename-item release-manifesta.yml release-manifest.yml
   get-content -path release-manifest.yml -raw
