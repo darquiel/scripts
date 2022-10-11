64701318bd5978c68047e6951343a94d32b835cc
mja <mark.andrews@serverobotics.com>
2022-10-10 19:35:18 -0700

    Removing script.

--- SRV/check-pr.ps1
+++
@@ -1,5 +0,0 @@
-push-location ~/serve/SRC/skel
-$PrList = gh pr list --search base:release/fleetware/3.32
-# write-host $PrList
-if ( $PrList -eq $null ) { Write-Host "No open PR's" } else { Write-Host "Open PR's" }
-pop-location
